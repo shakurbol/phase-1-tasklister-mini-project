a3a11f16f6cf92b3f47d951f5e78acecb8b63581
shakurbol <bolakokbol@gmail.com>
2023-10-02 08:06:03 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+const taskList = document.getElementById('tasks');
+
+form.addEventListener('submit', function (event) {
+  
+  event.preventDefault();
+
+  
+  const taskDescriptionInput = document.getElementById('new-task-description');
+  const taskDescription = taskDescriptionInput.value;
+
+  if (taskDescription.trim() !== '') {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskDescription;
+;
+    taskList.appendChild(taskItem);
+
+    
+    taskDescriptionInput.value = '';
+  }
 });
